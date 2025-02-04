2373c072473677e4dc69c1167c49a33ebff030fb
Alp <alpcevik19@gmail.com>
2025-02-05 02:36:27 +0300

    @Displayname added.

--- src/test/java/com/hostfully/tests/BookingCreationTests.java
+++ src/test/java/com/hostfully/tests/BookingCreationTests.java
@@ -6,7 +6,7 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
-@import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
